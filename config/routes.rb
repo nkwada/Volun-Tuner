c375a39b7c5665adcc29a3c515b9668b6fb0e47c
nkwada <n.qb.dft9@gmail.com>
2020-03-07 10:02:06 +0000

    [Add]ユーザー・アドミンのdeviseとネームスペース

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
+
+  devise_for :admin, controllers: {
+    registrations: 'admin/registrations',
+    sessions: 'admin/sessions'
+  }
+  namespace :admin do
+    resources :users
+  end
 end
