86a319b5592506651e2b5e130b086f6e5bfb20a8
nkwada <n.qb.dft9@gmail.com>
2020-03-10 04:45:02 +0000

    [Update]フォロー機能のレイアウト変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,14 +18,14 @@ class UsersController < ApplicationController
 	end
 
 	def following
-		@title = "Following"
+		@title = "フォロー"
 		@user  = User.find(params[:id])
 		@users = @user.following.paginate(page: params[:page])
 		render 'show_follow'
 	end
 
 	def followers
-		@title = "Followers"
+		@title = "フォロワー"
 		@user  = User.find(params[:id])
 		@users = @user.followers.paginate(page: params[:page])
 		render 'show_follow'
