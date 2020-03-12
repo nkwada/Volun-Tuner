cb80185c20c7e54cf73c3f2e192e5d9e3ce0e626
nkwada <n.qb.dft9@gmail.com>
2020-03-12 06:58:49 +0000

    [Add]管理画面にイベント一覧と削除機能を追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
-	belongs_to :user
-  	has_many :join_users
-  	has_many :joined_users, through: :join_users, source: :user
-    has_many :likes
- 	has_many :liked_users, through: :likes, source: :user
- 	has_many :comments
+	belongs_to :user, optional: true
+  	has_many :join_users, dependent: :destroy
+  	has_many :joined_users, through: :join_users, source: :user, dependent: :destroy
+    has_many :likes, dependent: :destroy
+ 	has_many :liked_users, through: :likes, source: :user, dependent: :destroy
+ 	has_many :comments, dependent: :destroy
  	mount_uploader :image, ImageUploader
  	geocoded_by :address
 	after_validation :geocode, if: :address_changed?
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   }
   namespace :admin do
     resources :users
+    resources :events, only: [:index, :destroy]
   end
 
   root to: 'users#top'
