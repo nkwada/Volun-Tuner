965caa1087b6efa45e88fa925d5bd9519f8e4a1d
nkwada <n.qb.dft9@gmail.com>
2020-03-17 07:52:14 +0000

    [Update]コメントの非同期化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,21 +2,24 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @comment = Comment.new(comment_params)
     @event = Event.find(params[:event_id])
+    @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
     @comment.event_id = @event.id
-    if @comment.save
-      redirect_back(fallback_location: root_path)
-    else
-      redirect_back(fallback_location: root_path)
-    end
+    @comment.save
+    # if @comment.save 非同期実装のためコメントアウト
+    #   redirect_back(fallback_location: root_path)
+    # else
+    #   redirect_back(fallback_location: root_path)
+    # end
   end
 
   def destroy
-	@comment = Comment.find(params[:event_id])
+  	@comment = Comment.find(params[:event_id])
+    @event = @comment.event
+    @comments = @event.comments
     @comment.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path) 非同期実装のためコメントアウト
   end
 
   private
