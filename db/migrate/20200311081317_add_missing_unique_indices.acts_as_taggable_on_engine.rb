da9fb3ee089e94530f8121b3c83677b9ede4d7ff
nkwada <n.qb.dft9@gmail.com>
2020-03-25 07:14:12 +0000

    [Fix]マイグレーションファイルの修正

--- db/migrate/20200311081317_add_missing_unique_indices.acts_as_taggable_on_engine.rb
+++ db/migrate/20200311081317_add_missing_unique_indices.acts_as_taggable_on_engine.rb
@@ -8,7 +8,7 @@ else
 end
 AddMissingUniqueIndices.class_eval do
   def self.up
-    add_index ActsAsTaggableOn.tags_table, :name, unique: true
+    # add_index ActsAsTaggableOn.tags_table, :name, unique: true
 
     if index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
       remove_index ActsAsTaggableOn.taggings_table, :tag_id
